92986bf449160337dcd83c6745d3ffda1721b0bc
RadhanpuraDeep <101540099+RadhanpuraDeep@users.noreply.github.com>
2022-08-09 20:09:58 +0530

    Add files via upload

---
+++ run_me.py
@@ -0,0 +1,21 @@
+from cv2 import threshold
+from Detector import *
+
+
+modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+#modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz"
+#modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz"
+#modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8.tar.gz"
+
+
+classFile = "coco.names"
+imagePath = "test/2.jpg"
+videoPath = 0
+threshold = 0.5
+
+detector = Detector()
+detector.readClasses(classFile)
+detector.downloadModel(modelURL)
+detector.loadModel()
+#detector.predictImage(imagePath, threshold)
+detector.predictVideo(videoPath, threshold)
